c562d8f01a17349e3320eeaf9d0c38e9c4d97ecf
Steven Ciraolo <steven.ciraolo@gmail.com>
2012-06-13 13:29:43 -0700

    working on fetchTweet function

--- public/javascripts/models.js
+++ public/javascripts/models.js
@@ -3,6 +3,7 @@ var Tweet = Backbone.Model.extend({
         return {
             user: null,
             id: null,
+            isPrimary: false,
             entities: null,
             geo: null,
             text: null,
@@ -11,7 +12,6 @@ var Tweet = Backbone.Model.extend({
         };
     },
     initialize: function() {
-            console.log('Tweet inited');
         }
 });
 
@@ -26,7 +26,6 @@ var Tweeter = Backbone.Model.extend({
         };
     },
     initialize: function () {
-            console.log('User inited');
     },
 });
 
@@ -45,15 +44,22 @@ var Convo = Backbone.Model.extend({
         this.primary = options.primary;
         this.conversants = new Tweeters();
         this.tweets = new Tweets();
-        console.log(this.tweets);
+        this.tweetsf = new Tweets();
+        this.tweetst = new Tweets();
+        //max_id parameter can be used to get all tweets up until given id
         this.urlTo = 'http://search.twitter.com/search.json?callback=?&rpp=100&q=to%3A'+ this.primary; 
         this.urlFrom = 'http://search.twitter.com/search.json?callback=?&rpp=100&q=from%3A'+ this.primary; 
+        this.conversantCount = 0;
         this.getAllTweets();
-        console.log(this.conversants);
-        this.getConversantCount();
     },
-    getConversantCount: function () {
-        this.conversantCount = this.conversants.length;
+    setConversantCount: function () {
+        this.conversantCount =  this.conversants.length;
+    },
+    getUrlToExtra: function() {
+        return this.urlTo+'&max_id='+_.last(this.filterTo()).get('id');
+    },
+    getUrlFromExtra: function() {
+        return this.urlFrom+'&max_id='+_.last(this.filterFrom()).get('id');
     },
     addTweetView: function(model) {
         var view = new TweetView({model:model});
@@ -63,64 +69,148 @@ var Convo = Backbone.Model.extend({
         var view = new TweeterView({model:model});
         $('#'+this.primary+'-info ul.conversants').append(view.render().el);
     },
-    getAllTweets: function() {
-        console.log(this.urlTo);
-        var self = this;
-        $.ajax({
-            url: this.urlTo,
-            dataType: 'jsonp',
-            error: function(error) {
-                console.log(error);
-            },
-            success: function(data) {
-                console.log(self.tweets);
-                data.results.forEach(function(t) {
-                    console.log(t);
-                    var tweet = new Tweet({user:t.from_user,
-                                         id: t.from_user_id,
-                                   entities:t.entities,
-                                        geo:t.geo,
-                                       text:t.text,
-                                         to:t.to_user,
-                                    created:t.created_at
-                                });
-                    self.tweets.add(tweet);
-                    console.log(self.tweets.length);
-                    self.addTweetView(tweet);
-                    console.log(t.user);
-                    var u = new Tweeter({username:t.from_user,
-                                               id: t.from_user_id,
-                                     profileImage: t.profile_image_url
-                                });
-                    console.log(u);
-                    var addUser = self.conversants.noDupeAdd(u);            
-                    if (addUser) {
-                        self.addTweeterView(u);
+    filterTo: function() {
+        console.log('filterTo called');
+        var list = _.filter(this.tweets.models, function(tweet) {
+            return !tweet.get('isPrimary');
+        });
+        return list;
+    },
+    filterFrom: function() {
+        console.log('filterFrom called');
+        var list = _.filter(this.tweets.models, function(tweet) {
+            return tweet.get('isPrimary');
+        });
+        return list;
+    },
+    //utility function to fetch tweets by given url parameter
+    fetchTo: function() {
+        console.log('fetchTo called');
+        var self = this,
+           count = 0;
+        var test = 'boo';
+        var getTweets = function (url, cb) {
+            console.log('ajax '+url);
+            $.ajax({
+                url: url,
+                dataType: 'jsonp',
+                error: function(error) {
+                    console.log(error);
+                },
+                success: function(data) {
+                    var tweet, i,t;
+                    count = data.results.length;
+                    console.log('js count '+count);
+                    for (i = 0; i < count; i++) {
+                        t = data.results[i];
+                        tweet = new Tweet({user:t.from_user,
+                                             id:t.from_user_id,
+                                      isPrimary:false,
+                                       entities:t.entities,
+                                            geo:t.geo,
+                                           text:t.text,
+                                             to:t.to_user,
+                                        created:t.created_at
+                                    });
+                        self.tweetst.add(tweet);
+                        var u = new Tweeter({username:t.from_user,
+                                                   id: t.from_user_id,
+                                         profileImage: t.profile_image_url
+                                    });
+                        var addUser = self.conversants.noDupeAdd(u);            
                     }
-                    console.log(self.conversants);
-                });
-                self.getConversantCount();
+                    console.log('t twts.lngth '+self.tweetst.length);
+                    console.log('is boo? '+test);
+                    console.log('t count.lngth '+count);
+                    cb();
+                }
+            });
+        };
+        function checkExtra () {
+            console.log('checkExtra called');
+            if ( count > 99) {
+                count = 0;
+                getTweets(self.getUrlToExtra(), checkExtra);
+            }
+            else {
+                count = 0;
+                self.renderReady.called.call(self);
+            }
+        }
+        getTweets(this.urlTo, checkExtra);
+    },
+    fetchFrom: function() {
+        console.log('fetchFrom called');
+        var self = this,
+           count = 0;
+        var getTweets = function (url, cb) {
+            $.ajax({
+                url: url,
+                dataType: 'jsonp',
+                error: function(error) {
+                    console.log(error);
+                },
+                success: function(data) {
+                    count = data.results.length;
+                    console.log('jsf count '+count);
+                    data.results.forEach(function(t) {
+                        var tweet = new Tweet({user:t.from_user,
+                                             id: t.from_user_id,
+                                      isPrimary: true,
+                                       entities:t.entities,
+                                            geo:t.geo,
+                                           text:t.text,
+                                             to:t.to_user,
+                                        created:t.created_at
+                                    });
+                        self.tweetsf.add(tweet);
+                    });
+                    count = data.results.length;
+                    console.log('f twts.lngth '+self.tweetsf.length);
+                    console.log('f count.lngth '+data.results.length);
+                    cb();
+                }
+            });
+        };
+        var checkExtra = function() {
+            console.log('checkExtra called');
+            if ( count > 99) {
+                count = 0;
+                getTweets(self.getUrlFromExtra(), checkExtra);
+            }
+            else {
+                count = 0;
+                self.renderReady.called.call(self);
+            }
+        };
+        getTweets(this.urlFrom, checkExtra);
+    },
+    renderReady: (function() {
+        var returned = 0;
+        return ({called: function () {
+            var self = this;
+            console.log('renderReady called '+returned+'-'+new Date().getTime());
+                if (returned === 0) {
+                    returned = 1;
+                }
+                else {
+                    this.tweets.forEach(function(tweet) {
+                        self.addTweetView(tweet);
+                    });
+                    this.setConversantCount();
+                    console.log('rr length '+this.conversantCount);
+                    console.log('tww length '+this.tweets.length);
+                    var cview = new ConvoView({model:this});
+                    console.log(cview.render().el);
+                    $('#convos-list').append(cview.render().el);
+                    returned = 0;
+                }
             }
         });
-        $.ajax({
-            url: this.urlFrom,
-            dataType: 'jsonp',
-            error: function(error) {
-                console.log(error);
-            },
-            success: function(data) {
-                data.results.forEach(function(t) {
-                    var t = new Tweet({user:t.from_user,
-                                         id: t.from_user_id,
-                                   entities:t.entities,
-                                        geo:t.geo,text:t.text,
-                                         to:t.to_user,
-                                    created:t.created_at});
-                    self.tweets.add(t);
-                    self.addTweetView(t);
-                });
-            }
-        }); 
+    })(),
+    getAllTweets: function() {
+        this.fetchTo(),
+        this.fetchFrom();
     }
 });
 
@@ -130,6 +220,9 @@ var Convos = Backbone.Collection.extend({
 
 var Tweets = Backbone.Collection.extend({
     model: Tweet,
+    comparator: function(tweet) {
+        return tweet.get('created');
+    }
 });
 
 var Tweeters = Backbone.Collection.extend({
@@ -174,6 +267,7 @@ var ConvoView = Backbone.View.extend({
     tagName: "li",
     template: _.template($('#convo-tmpl').html()),
     initialize: function() {
+        console.log('convoview inited');
         this.render();
     },
     render: function() {
@@ -206,8 +300,6 @@ var AppView = Backbone.View.extend({
         $('#convo-box').append(ul);
         var convo = new Convo({primary:term});
         this.convos.add(convo);
-        var view = new ConvoView({model:convo});
-        $('#convos-list').append(view.render().el);
     },
     searchTermSubmit: function(e) {
         console.log('searchTermSubmit called');
